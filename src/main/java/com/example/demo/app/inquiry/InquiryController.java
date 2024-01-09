95edd6b1a18435102ce29cb42b354617b08dbcf5
Shocker55 <bialetti360@gmail.com>
2024-01-09 13:42:43 +0900

    feat: @Validatedを使いフォームに検証の追加、確認画面の作成

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InquiryController {
 
     @GetMapping("/form")
-    public String form(Model model) {
+    public String form(InquiryForm inquiryForm, Model model) {
         model.addAttribute("title", "Inquiry Form");
         return "inquiry/form";
+    }
+
+    // confirm画面でgo backした時に表示させるページのルーティング
+    @PostMapping("/form")
+    public String formGoBack(InquiryForm inquiryForm, Model model) {
+        model.addAttribute("title", "Inquiry Form");
+        return "inquiry/form";
+    }
+
+    @PostMapping("/confirm")
+    // @Validated をつけることでアノテーションで@NotNull, @Email等した所に検査がかかるようになる
+    // BindingResult には Validationnの結果が格納
+    public String confirm(@Validated InquiryForm inquiryForm,
+                          BindingResult result,
+                          Model model) {
+        if(result.hasErrors()) {
+           model.addAttribute("title", "Inquiry Form");
+           return "inquiry/form";
+        }
+        model.addAttribute("title", "Confirm Page");
+        return "inquiry/confirm";
     }
 }
