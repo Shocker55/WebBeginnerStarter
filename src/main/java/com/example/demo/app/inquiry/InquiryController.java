b1ca7491c594283c9852bbd77886a318e8170d76
Shocker55 <bialetti360@gmail.com>
2024-01-09 14:20:41 +0900

    feat: 完了処理の作成 リダイレクトとフラッシュスコープ

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -5,15 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
 
     @GetMapping("/form")
-    public String form(InquiryForm inquiryForm, Model model) {
+    public String form(InquiryForm inquiryForm,
+                       Model model,
+                       // フラッシュスコープの値を受け取るために引数の追加
+                       // @ModelAttribute(フラッシュスコープのキーの名前)
+                       @ModelAttribute("complete") String complete) {
         model.addAttribute("title", "Inquiry Form");
         return "inquiry/form";
     }
@@ -27,7 +33,7 @@ public class InquiryController {
 
     @PostMapping("/confirm")
     // @Validated をつけることでアノテーションで@NotNull, @Email等した所に検査がかかるようになる
-    // BindingResult には Validationnの結果が格納
+    // BindingResult には Validationの結果が格納
     public String confirm(@Validated InquiryForm inquiryForm,
                           BindingResult result,
                           Model model) {
@@ -37,5 +43,26 @@ public class InquiryController {
         }
         model.addAttribute("title", "Confirm Page");
         return "inquiry/confirm";
+    }
+
+    @PostMapping("/complete")
+    // RedirectAttributesはフラッシュスコープを使うために設定(ページ間を超えてデータ操作するためにセッションを利用)
+    // また、DB保存後の2重クリックを防止するためにリダイレクトする(セッションで保持した値を使って完了させる)役割もある。
+    // Udemy5-30, 5-31 に詳細
+    public String complete(@Validated InquiryForm inquiryForm,
+                           BindingResult result,
+                           Model model,
+                           RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("title", "Inquiry Form");
+            return "inquiry/form";
+        }
+
+        // dbとの処理 (今は省略)
+
+        // 一度下記で設定したRegisteredが画面に表示されたらセッションの情報は削除されるため、フラッシュスコープと呼ぶ
+        redirectAttributes.addFlashAttribute("complete", "Registered");
+        // ここのreturnはhtmlではなく、urlを指している
+        return "redirect:/inquiry/form";
     }
 }
