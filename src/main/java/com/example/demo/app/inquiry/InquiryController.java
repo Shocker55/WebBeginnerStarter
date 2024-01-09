92e6d3cfb9631f3e6c7c47429371677e6c605863
Shocker55 <bialetti360@gmail.com>
2024-01-09 23:26:53 +0900

    feat: サービスクラスの作成

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -1,5 +1,8 @@
 package com.example.demo.app.inquiry;
 
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
+
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
+
+    private final InquiryService inquiryService;
+
+    @Autowired
+    public InquiryController(InquiryService inquiryService) {
+        this.inquiryService = inquiryService;
+    }
 
     @GetMapping("/form")
     public String form(InquiryForm inquiryForm,
@@ -58,8 +70,15 @@ public class InquiryController {
             return "inquiry/form";
         }
 
-        // dbとの処理 (今は省略)
+        // dbとの処理
+        // まずはInquiryFormというクラスからInquiryというEntityクラスに値を詰め替える
+        Inquiry inquiry = new Inquiry();
+        inquiry.setName(inquiryForm.getName());
+        inquiry.setName(inquiryForm.getEmail());
+        inquiry.setContents(inquiryForm.getContents());
+        inquiry.setCreated(LocalDateTime.now());
 
+        inquiryService.save(inquiry);
         // 一度下記で設定したRegisteredが画面に表示されたらセッションの情報は削除されるため、フラッシュスコープと呼ぶ
         redirectAttributes.addFlashAttribute("complete", "Registered");
         // ここのreturnはhtmlではなく、urlを指している
