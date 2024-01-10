5f91a611a35d638a3baf1e69c503f998d35c3848
Shocker55 <bialetti360@gmail.com>
2024-01-10 16:52:22 +0900

    feat: アンケートフォームデータベース操作演習 完成形

---
+++ src/main/java/com/example/demo/repository/SurveyDao.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Survey;
+
+import java.util.List;
+
+public interface SurveyDao {
+
+    void insertSurvey(Survey survey);
+
+    List<Survey> getAll();
+}
